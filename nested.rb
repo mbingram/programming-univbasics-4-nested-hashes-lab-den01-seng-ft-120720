79583c5a1e795004be71871438cca82076a038a3
charming-input-2976 <mbingramm@gmail.com>
2020-11-25 19:46:32 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,8 @@ def hopper
         :languages => ["C"]
       }
     }
+ programmer_hash[:grace_hopper]
+ 
 
 end
 
@@ -36,6 +38,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+ programmer_hash[:alan_kay][:known_for]
+ 
 
 end
 
@@ -55,7 +59,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,8 +83,13 @@ def adding_matz
       :dennis_ritchie => {
         :known_for => "Unix",
         :languages => ["C"]
+      },
+      :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
       }
     }
+  programmer_hash
 
 end
 
@@ -103,6 +112,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
 
 end
 
@@ -125,5 +136,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+programmer_hash
 end
